e2da9658e8226e5363f42c557da2696d2ac1a2e4
Rizky-wp <widiputrorizky@gmail.com>
2021-04-15 09:19:08 +0700

    navbar sticky top

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -65,7 +65,7 @@
 
         <!-- Page Content Holder -->
         <div id="content" class="visible">
-            <div class="navbar navbar-light">
+            <nav class="navbar sticky-top navbar-light bg-light">
                 <div class="container-fluid">
                     <div id="btn-sidebar">
                         <span></span>
@@ -73,10 +73,10 @@
                         <span></span>
                     </div>
                 </div>
-            </div>
+            </nav>
+            <p style="font-size: 5em;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas modi dolor assumenda eos reprehenderit cum quisquam consequatur maiores quae aut laboriosam alias rerum, ad itaque ex recusandae quasi autem! Doloremque.</p>
             <?= $this->renderSection('content'); ?>
         </div>
-    </div>
 
 
 
@@ -86,13 +86,13 @@
 
 
 
-    <!-- Optional JavaScript; choose one of the two! -->
+        <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
     -->
