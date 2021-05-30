8b68fa502b103b4db2f6ae5b025994fb080b3779
YusufRangkuti <rangkuti289@gmail.com>
2021-05-30 19:12:13 +0700

    Rapihin Episode

--- app/Views/search/episode.php
+++ app/Views/search/episode.php
@@ -3,35 +3,34 @@
 <script src="/js/main.js"></script>
 <?= $this->endSection('js'); ?>
 <?= $this->section('content'); ?>
-<div class=" d-flex justify-content-between bg">
-    <div class=" d-flex flex-row">
-        <div class="p-3 bd-highlight cover">
-            <img src="<?= $data_search->images[0]->url; ?>" alt="">
-        </div>
-        <div class="p-3 bd-highlight">
-            <h4>PODCAST</h4>
-            <h1><?= $data_search->name; ?></h1>
-            <h3><?= $data_search->publisher; ?></h3>
-        </div>
-    </div>
-</div>
-<div class=" d-flex justify-content-between bg2">
-
-    <div class=" d-flex flex-row ">
-        <div class="p-3 bd-highlight">
-            <h3>All Episodes </h3>
+<div class="container-fluid bg">
+    <div class=" d-flex justify-content-between">
+        <div class=" d-flex flex-row">
+            <div class="p-3 bd-highlight cover">
+                <img src="<?= $data_search->images[0]->url; ?>" alt="">
+            </div>
+            <div class="p-3 bd-highlight">
+                <h1><?= $data_search->name; ?></h1>
+                <h3><?= $data_search->publisher; ?></h3>
+            </div>
+            <div class="p-3 bd-highlight">
+                <h4> Deskripsi </h4>
+            </div>
         </div>
     </div>
-
-</div>
-<input type="hidden" id="id_pod" value="<?= $data_search->id; ?>"></input>
-<input type="hidden" id="page" value="<?= $page; ?>"></input>
-<?php foreach ($data_episode->items as $episodes) : ?>
-    <div class="list-group episode">
-        <a href="#" class="list-group-item list-group-item-action"><?= $episodes->name; ?></a>
+    <div class="p-3 bd-highlight">
+        <h2>All Episodes </h2>
     </div>
-<?php endforeach; ?>
-<div id="tambah" class="">
-    <button class="button" id="load_button" onclick="load_click()">load more</button>
+    <input type="hidden" id="id_pod" value="<?= $data_search->id; ?>"></input>
+    <input type="hidden" id="page" value="<?= $page; ?>"></input>
+    <?php foreach ($data_episode->items as $episodes) : ?>
+        <div class="list-group episode">
+            <a href="#" class="list-group-item list-group-item-action">
+                <img src="/img/arts.png" alt=""><?= $episodes->name; ?></a>
+        </div>
+    <?php endforeach; ?>
+    <div id="tambah" class="">
+        <button type="button" class="btn btn-secondary" onclick="load_click()">Secondary</button>
+    </div>
+    <?= $this->endSection(); ?>
 </div>
-<?= $this->endSection(); ?>
