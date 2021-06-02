c644e7d6d37f9071ab743595b7b02b9fd28be294
YusufRangkuti <rangkuti289@gmail.com>
2021-06-02 16:04:10 +0700

    COMMENT SECTION

--- app/Views/comment/comment.php
+++ app/Views/comment/comment.php
@@ -3,21 +3,23 @@
 <?= $this->section('content'); ?>
 <div class="container-fluid mt-5">
     <img src="<?= $data_episode->images[0]->url; ?>" class="rounded mx-auto d-block player-cover" alt="...">
-    <iframe src="https://open.spotify.com/embed/episode/<?= $data_episode->id; ?>" width="100%" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    <iframe src="https://open.spotify.com/embed/episode/<?= $data_episode->id; ?>" width="100%" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media" class="player"></iframe>
 </div>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col">
-            <img src="/img/arts.png" alt="">
-        </div>
-    </div>
+<div class="container-fluid comment">
     <div class="row">
         <div class="col">
             <input type="comment" class="form-control mt-2  " id="comment">
         </div>
     </div>
-    <button type="submit" class="btn btn-primary mt-2">Submit</button>
+    <button type="submit" class="btn btn-primary btncomment mt-2">Submit</button>
+    <div class="card mt-3">
+        <div class="card-header">
+            Nickname
+        </div>
+        <div class="card-body">
+            <h4>ISI COMMENT</h4>
+        </div>
+    </div>
 </div>
-
 
 <?= $this->endSection(); ?>
